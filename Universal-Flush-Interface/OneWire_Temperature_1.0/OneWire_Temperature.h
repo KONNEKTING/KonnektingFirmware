38fc22383dcdcffb5810fd0d3a53aefb2d5718d3
Adminius <eugenius707@gmail.com>
2016-08-07 12:08:18 +0200

    Update OneWire_Temperature.h

--- Universal-Flush-Interface/OneWire_Temperature_1.0/OneWire_Temperature.h
+++ Universal-Flush-Interface/OneWire_Temperature_1.0/OneWire_Temperature.h
@@ -4,6 +4,8 @@
 #define REVISION 0
 
 //Define OneWire connection. Please check ufi_rev1.h
-#define ONE_WIRE OWLeft 
-#define ONE_WIRE_PULLUP OWLeftPullUp
+//#define ONE_WIRE OWLeft 
+//#define ONE_WIRE_PULLUP OWLeftPullUp
 
+#define ONE_WIRE OWRight 
+#define ONE_WIRE_PULLUP OWRightPullUp
